f9d3adf042a38a189b74c8a6ca154911a05be575
Stepan Isayev <140114944+isastep@users.noreply.github.com>
2024-05-07 21:52:26 +0200

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,58 @@
+
+    for item in data:
+def generate_random_data():
+
+def main():
+    main()
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+        print(f"Random Number: {item}")
+    return data
+import random
+    data = generate_random_data()
+
+    }
+}
+    public static void main(String[] args) {
+        int[] data = new int[10];
+            System.out.println("Random Number: " + item);
+        }
+        for (int i = 0; i < 10; i++) {
+            data[i] = (int) (Math.random() * 100) + 1;
+        for (int item : data) {
+
+
+        }
+public class RandomDataGenerator {        data.push(Math.floor(Math.random() * 100) + 1);
+    return data;
+    const data = [];
+    data.forEach(item => {
+    }
+    });
+
+main();
+    for (let i = 0; i < 10; i++) {
+    const data = generateRandomData();
+
+function main() {
+        console.log("Random Number: " + item);
+}
+function generateRandomData() {
+}
+}
+function main() {
+    for (let i = 0; i < 10; i++) {
+    });
+    }
+    data.forEach(item => {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    const data = [];
+
+        console.log("Random Number: " + item);
+main();
+function generateRandomData() {
+
+}
+    return data;
+
+    const data = generateRandomData();
